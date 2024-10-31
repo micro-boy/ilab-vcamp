1b0cf202ac99526d043c059b87bf9a724ced41ad
micro-boy <antonprafanto@unmul.ac.id>
2024-10-31 17:34:35 +0800

    main

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\RouteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home.index');
+    return view('home.index-4');
 });
 
 Route::get('{any}', [RouteController::class, 'index'])->where('any', '.*');
